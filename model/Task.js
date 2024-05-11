4386d2fe4f5894f20539abd357148ce862629eba
jebi2420 <jebi2420@gmail.com>
2024-05-11 21:57:22 +0900

    [add] DB 스키마 정의, 모델 생성

---
+++ model/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// 스키마
+const taskSchema = Schema({
+    task:{
+        type: String,
+        required: true
+    },
+    isComplete:{
+        type: Boolean,
+        required: true
+    }
+})
+
+// 모델
+const Task = mongoose.model("Task", taskSchema)
+
+module.exports = Task;
