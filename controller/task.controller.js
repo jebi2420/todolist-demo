889862d089ea38b4065d4cea518d37ac1f023771
jebi2420 <jebi2420@gmail.com>
2024-05-11 22:47:29 +0900

    [add] post, get 기능

---
+++ controller/task.controller.js
@@ -0,0 +1,27 @@
+const Task = require('../model/Task');
+
+const taskController = {}
+
+// 할 일을 추가하는 기능
+taskController.createTask = async (req, res) => {
+    try{
+        const { task, isComplete } = req.body;
+        const newTask = new Task({ task, isComplete });
+        await newTask.save();
+        res.status(200).json({ status:'ok', data: newTask });
+    }catch(err){
+        res.status(400).json({ status: 'fail', error: err });
+    }
+};
+
+// 할 일 리스트를 볼 수 있는 기능
+taskController.getTask = async (req, res) => {
+    try{
+        const taskList = Task.find({}) // 테스크 항목을 다 읽어온다
+        res.status(200).json({ status: "ok", data: taskList})
+    }catch(err){
+        res.status(400).json({ status: 'fail', error: err })
+    }
+}
+
+module.exports = taskController
