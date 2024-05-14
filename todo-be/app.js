adc2e4a27235ca543b9b25b0f0b8f48c4f92f1e8
jebi2420 <jebi2420@gmail.com>
2024-05-14 18:05:32 +0900

    [add] 백엔드 cors 설정

--- todo-be/app.js
+++ todo-be/app.js
@@ -2,8 +2,10 @@ const express = require ('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require("./routes/index");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
