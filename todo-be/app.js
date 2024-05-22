bbe55d26fd62096093f2b9de62598fa7dfe9a7ab
jebi2420 <jebi2420@gmail.com>
2024-05-22 12:21:59 +0900

    [add] 주석추가

--- todo-be/app.js
+++ todo-be/app.js
@@ -23,3 +23,13 @@ mongoose.connect(mongoURI, {useNewUrlParser:true}).then(()=>{
 app.listen(process.env.PORT || 5001, ()=> {
     console.log("server on 5001")
 });
+
+// 1. 회원가입
+// 유저가 이메일, 패스워드, 유저이름 입력해서 보냄
+// 받은 정보를 저장함 (데이터베이스 모델 필요)
+// 패스워드를 암호화 시켜서 저장
+
+// 1. 라우터
+// 2. 모델
+// 3. 데이터를 저장(이미 가입된 유저 유무, 패스워드 암호화)
+// 4. 응답을 보낸다(ok sign)
