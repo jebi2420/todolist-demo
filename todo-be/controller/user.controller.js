c8af8b8e208b853b4750ee5aeb947ecc43f44bf2
jebi2420 <jebi2420@gmail.com>
2024-05-22 16:54:42 +0900

    [fix] 오타 수정
    sign in

--- todo-be/controller/user.controller.js
+++ todo-be/controller/user.controller.js
@@ -33,7 +33,7 @@ userController.loginWithEmail = async(req, res)=>{
         // 유저가 입력한 이메일, 패스워드 정보 읽어오기
         const {email, password} = req.body
         // 이메일을 가지고 유저정보 가져오기
-        const user = await User.findOne({email});
+        const user = await User.findOne({email}, "-createdAt -updatedAt -__v");
         // 이 유저에 디비에 있는 패스워드와 프엔에서 보낸 패스워드가 같은지 비교
         if(user){
             const isMatch = bcrypt.compareSync(password, user.password); 
@@ -45,7 +45,7 @@ userController.loginWithEmail = async(req, res)=>{
         }
         throw new Error ("아이디 또는 비밀번호가 일치하지 않습니다")
     }catch(error){
-        res.status(400).json({status:"signup fail", error: error.message})
+        res.status(400).json({status:"sign in fail", error: error.message})
     }
 }
 
