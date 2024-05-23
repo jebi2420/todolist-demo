9c3a108cca52b192293bdd114c5fa7d756f6de12
jebi2420 <jebi2420@gmail.com>
2024-05-23 22:42:20 +0900

    [fix] user error 핸들링 수정

--- todo-be/controller/user.controller.js
+++ todo-be/controller/user.controller.js
@@ -45,7 +45,7 @@ userController.loginWithEmail = async(req, res)=>{
         }
         throw new Error ("아이디 또는 비밀번호가 일치하지 않습니다")
     }catch(error){
-        res.status(400).json({status:"sign in fail", error: error.message})
+        res.status(400).json({status:"sign in fail", message: error.message})
     }
 }
 
