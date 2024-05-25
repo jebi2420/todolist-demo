f923ce524cd145b76d72f2d19a5ba8f638be99d3
jebi2420 <jebi2420@gmail.com>
2024-05-25 18:01:47 +0900

    [add] 토큰으로 유저정보 찾기
    미들웨어 사용

--- todo-be/routes/user.api.js
+++ todo-be/routes/user.api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const authController = require('../controller/auth.controller');
 const router = express.Router();
 const userController = require("../controller/user.controller");
 
@@ -6,5 +7,7 @@ const userController = require("../controller/user.controller");
 router.post("/", userController.createUser);
 // 2. 로그인 endpoint
 router.post("/login", userController.loginWithEmail);
+// 토큰을 통해 유저 id 빼내고 => 그 아이디로 유저 객체 찾아서 보내주기
+router.get("/me", authController.authenticate, userController.getUser);
 
 module.exports = router;
